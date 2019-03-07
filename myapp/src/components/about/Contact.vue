b3620336611c5462bcab9b0792c3ac52c824a70c
myweb <614677970@qq.com>
2019-03-07 16:30:50 +0800

    second commit

---
+++ myapp/src/components/about/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contact">
+    <h1>这是Contact的页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      
+    }
+  }
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
